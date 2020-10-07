210604b8ccef3c4b4e0ed5ce437b5aebf05e4d31
HannePeter <hannepeter@posteo.de>
2020-10-07 18:11:48 +0200

    update helper / self checking testbench

--- helper/run_simulation.sh
+++ helper/run_simulation.sh
@@ -5,14 +5,16 @@ tb=$(ls | grep _tb.vhd | cut -f 1 -d '.' | tr [:upper:] [:lower:])
 
 for file in $files; do
     echo "analyze $file ... "
-    ghdl -a $file
+    ghdl -a --std=08 $file
 done
 
 echo "elaborate $tb ... "
-ghdl -e $tb
+ghdl -e --std=08 $tb
 
 echo "run simulation $tb"
 ghdl -r $tb --vcd=simulation.vcd
+
+echo ""
 gtkwave simulation.vcd
 
 rm -f *.o *.cf simulation.vcd
--- helper/verify.sh
+++ helper/verify.sh
@@ -4,8 +4,8 @@ files=$(ls | grep .vhd)
 
 for file in $files; do
     echo "verify $file ... "
-    ghdl -s $file
-    ghdl -a $file
+    ghdl -s --std=08 $file
+    ghdl -a --std=08 $file
 done
 
 rm -f *.o *.cf
