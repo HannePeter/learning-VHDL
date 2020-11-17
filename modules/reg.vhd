81a38de580500cf1109da2a9df7fc224f0f5a64d
HannePeter <hannepeter@posteo.de>
2020-11-17 15:15:25 +0100

    use pos edge only

--- modules/reg.vhd
+++ modules/reg.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 
 entity reg is
     generic (
-       n : integer := 8 
+        n : integer := 8 
     );
     port (
         clk      : in  std_logic;
@@ -18,28 +18,21 @@ end;
 
 architecture behave of reg is
 
-    signal r_temp   : std_logic_vector(n-1 downto 0);
+    signal r_out    : std_logic_vector(n-1 downto 0);
 
 begin
 
-    process(clk, reset_n)                   -- first (input) FF, loaded with pos clk edge
+    process(clk, reset_n)
     begin
         if (reset_n = '0') then
-            r_temp <= (others => '0');
+            r_out <= (others => '0');
         elsif rising_edge(clk) then
             if (enable = '1') then
-                r_temp <= in_data;
+                r_out <= in_data;
             end if;
         end if;
     end process;
 
-    process(clk, reset_n)                   -- second (output) FF, loaded with neg clk edge
-    begin
-        if (reset_n = '0') then
-            out_data <= (others => '0');
-        elsif falling_edge(clk) then
-            out_data <= r_temp;
-        end if;
-    end process;
+    out_data <= r_out;
 
 end;
